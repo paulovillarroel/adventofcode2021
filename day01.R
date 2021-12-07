4d823ed8f415faf395f7dc7f293824ba312cd6ba
Paulo Villarroel <pvillarroeltapia@gmail.com>
2021-12-06 23:27:32 -0300

    Upload day 1

---
+++ day01.R
@@ -0,0 +1,9 @@
+## Day 01 Sonar Sweep
+library(tidyverse)
+data <- read.csv("data/day01.csv", col.names = "depth")
+
+# Part 1
+sum(data$depth > lag(data$depth), na.rm = TRUE)
+
+# Part 2
+sum(data$depth > lag(data$depth, n = 3), na.rm = TRUE)
